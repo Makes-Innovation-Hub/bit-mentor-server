50f895d3d2d10f8caa951a2c3e80f4ac907fff2f
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2024-07-25 12:30:52 +0300

    Update MongoDb.py

--- model/MongoDb.py
+++ model/MongoDb.py
@@ -12,24 +12,25 @@ def get_db():
 
 class MongoDatabase:
     def __init__(self):
-        self.client = self._check_mongo_connection()
+        self.client = _check_mongo_connection()
         self.db = self.client["QuestDB"]
         self.questions_collection = self.db["Questions"]
         self.users_collection = self.db["Users"]
         self.topics_collection = self.db["Topics"]
 
-    def _check_mongo_connection(self):
-        username = os.getenv("MONGO_USERNAME")
-        password = os.getenv("MONGO_PASSWORD")
-        cluster_url = os.getenv("MONGO_CLUSTER_URL")
 
-        if not all([username, password, cluster_url]):
-            raise EnvironmentError("MongoDB credentials are not set.")
+def _check_mongo_connection():
+    username = os.getenv("MONGO_USERNAME")
+    password = os.getenv("MONGO_PASSWORD")
+    cluster_url = os.getenv("MONGO_CLUSTER_URL")
 
-        connection_string = f"mongodb+srv://{username}:{password}@{cluster_url}"
-        try:
-            client = MongoClient(connection_string)
-            client.admin.command('ping')
-            return client
-        except Exception as e:
-            raise ConnectionError(f"Failed to connect to MongoDB: {str(e)}")
+    if not all([username, password, cluster_url]):
+        raise EnvironmentError("MongoDB credentials are not set.")
+
+    connection_string = f"mongodb+srv://{username}:{password}@{cluster_url}"
+    try:
+        client = MongoClient(connection_string)
+        client.admin.command('ping')
+        return client
+    except Exception as e:
+        raise ConnectionError(f"Failed to connect to MongoDB: {str(e)}")
