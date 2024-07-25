3092f2651519e8e2a1b79061fa63c11574622ec4
yishain11 <yishain11@walla.co.il>
2024-07-25 15:25:24 +0300

    refactored the openai prompt functions and endpoint

--- server/controllers/openai_controller.py
+++ server/controllers/openai_controller.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter, HTTPException
-from server.utils.open_ai import get_question_and_answer_and_explanation
+from fastapi import APIRouter, Response,status
+from server.utils.open_ai import get_openai_response
+from server.utils.ai_prompt import generate_question_prompt
+from data_types.gen_question_body import GenQuestionBody
 
 router = APIRouter()
 
-@router.get("/generate-question/{topic}")
-def get_generated_question(topic: str):
-    result = get_question_and_answer_and_explanation(topic)
-    if "error" in result:
-        raise HTTPException(status_code=400, detail=result["error"])
-    return result
+@router.post("/generate-question")
+def generate_question(body: GenQuestionBody, response :Response):
+    try:
+        prompt = generate_question_prompt(body.topic,body.difficulty)
+        result = get_openai_response(prompt)
+        response.status_code = status.HTTP_200_OK
+        return result
+    except KeyError as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return e
+    except Exception as e:
+        print('e: ', e)
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return e
+        
--- tests/test_open_ai.py
+++ tests/test_open_ai.py
@@ -1,11 +1,10 @@
 from server.utils.open_ai import get_openai_response
-from server.utils.ai_prompt import create_question_and_answer_and_explanation
-import pytest
+from server.utils.ai_prompt import generate_question_prompt
 from fastapi.testclient import TestClient
 from server.server import app
 
 def test_get_openai_response_contains_keys():
-    prompt = create_question_and_answer_and_explanation("python")
+    prompt = generate_question_prompt("python")
     response = get_openai_response(prompt)
     assert isinstance(response, dict)
     assert "question" in response
@@ -16,10 +15,11 @@ def test_get_openai_response_contains_keys():
 client = TestClient(app)
 
 def test_generate_question():
-    response = client.get("/generate-question/science")
+    response = client.post("/generate-question",json={"topic":"python"},)
     assert response.status_code == 200
     result = response.json()
     assert "question" in result
     assert "answer" in result
     assert "explanation" in result
     assert len(result["question"]) <= 100
+    assert "python" in result["question"].lower()
