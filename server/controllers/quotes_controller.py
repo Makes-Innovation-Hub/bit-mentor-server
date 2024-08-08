c48fdcc237cc901375398717fb199938f42fef1a
najeebib <najeebibraheem1999@hotmail.com>
2024-08-08 17:53:37 +0300

    Update quotes_controller.py
    updated endpoint
    get 5 quotes check if user didnt see one and send it
    if there are no quotes user didnt see get one from API

--- server/controllers/quotes_controller.py
+++ server/controllers/quotes_controller.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter, HTTPException
 import requests
-
+import model.select_queries as select_queries
+import model.insert_queries as insert_queries
 from setting.config import *
+from model.MongoDb import MongoDatabase
+from server.utils.logger import app_logger
 
 router = APIRouter()
+
+mongo_uri = config.MONGO_CLUSTER
+database_name = config.DATABASE_NAME
+mongo_db = MongoDatabase(mongo_uri, database_name)
+quotes_collection = mongo_db.quotes_collection
 
 @router.get("/quote/{user_id}")
 def get_quote(user_id: int):
@@ -18,10 +26,23 @@ def get_quote(user_id: int):
     Raises:
         HTTPException: If the response status code is not 200.
     """
+    quotes = select_queries.get_random_quotes(quotes_collection)
+    for quote in quotes:
+        if user_id not in quote['user_ids']:
+            insert_queries.add_user_to_quote(quotes_collection, quote['_id'], user_id)
+            return quote['quote']
+
     api_url = 'https://api.api-ninjas.com/v1/quotes'
     response = requests.get(api_url, headers={'X-Api-Key': config.MOTIVATION_API})
-
+    
     if response.status_code == requests.codes.ok:
-        return response.json()[0]
+        new_quote = response.json()[0]
+        insert_queries.insert_quote(quotes_collection, new_quote)
+        quote_doc = quotes_collection.find_one({'quote': new_quote})
+        if quote_doc:
+            insert_queries.add_user_to_quote(quotes_collection, quote_doc['_id'], user_id)
+            return new_quote
+
     else:
+        app_logger.warning("Failed to retrieve a new quote from the API")
         raise HTTPException(status_code=400, detail="Error")
