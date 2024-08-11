f1fefcaee70d84fd394d7ea629dee098ce322e10
maysazu4 <77391084+maysazu4@users.noreply.github.com>
2024-08-11 15:13:20 +0300

    Update MongoDb.py

--- model/MongoDb.py
+++ model/MongoDb.py
@@ -19,7 +19,7 @@ class MongoDatabase:
         self.stats_collection = self.db["stats"]
         self.topics = self.db["allowed_topics"]
         self.init_topics()
-        
+
     def init_topics(self):
         try:
             # Fetch existing topics
@@ -32,11 +32,7 @@ class MongoDatabase:
             # Insert missing topics
             if missing_topics:
                 self.topics.insert_many([{"name": topic} for topic in missing_topics])
-                app_logger.info(f"Inserted missing topics: {missing_topics}")
-            else:
-                app_logger.info("All topics are already present.")
         except Exception as e:
-            app_logger.error(f"Error initializing topics: {e}")
             raise RuntimeError("Failed to initialize topics") from e
 
     def insert_question(self,collection_name,question_data):
