416ba194a8b9b3f0dfcbe1100afbac27358d6a17
najeebib <najeebibraheem1999@hotmail.com>
2024-08-11 13:42:50 +0300

    Update test_quotes_controller.py

--- tests/test_quotes_controller.py
+++ tests/test_quotes_controller.py
@@ -1,21 +1,12 @@
 from fastapi.testclient import TestClient
 from server.server import app  # make sure to import your FastAPI app
-from setting.config import *
-from model.MongoDb import MongoDatabase
 
 client = TestClient(app)
+
 def test_get_quote_success():
-    response = client.get("/quote/1")
+    response = client.get("/quote/123")
     assert response.status_code == 200
     response_json = response.json()
-
-    mongo_uri = config.MONGO_CLUSTER
-    database_name = config.DATABASE_NAME
-    mongo_db = MongoDatabase(mongo_uri, database_name)
-    quotes_collection = mongo_db.quotes_collection
-    quote_doc = quotes_collection.find_one({'quote': response_json})
-    if quote_doc:
-        result = quotes_collection.update_one(
-                {'_id': quote_doc['_id']},
-                {'$pull': {'user_ids': 1}}
-        )
+    assert "quote" in response_json
+    assert "author" in response_json
+    assert "category" in response_json
